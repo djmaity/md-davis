5e11ac037a0f6ddb681b8bb83b854689772f6d9c
Dibyajyoti Maity <13766712+djmaity@users.noreply.github.com>
2020-04-10 19:27:52 +0530

    MD DaVis Version 0.2.0

--- md_davis/__init__.py
+++ md_davis/__init__.py
@@ -3,7 +3,7 @@
 
 __author__ = """Dibyajyoti Maity"""
 __email__ = 'djdibs@gmail.com'
-__version__ = '0.1.0'
+__version__ = '0.2.0'
 name = "md_davis"
 __all__ = ["collect_data",
            "electrostatics",
--- setup.py
+++ setup.py
@@ -59,7 +59,7 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://djmaity.github.io/md_davis/',
-    version='0.1.0',
+    version='0.2.0',
     zip_safe=False,
     python_requires='>=3.6',
 )
