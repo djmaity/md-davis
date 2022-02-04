062461685292fb06a22af9f794900b9423384273
Dibyajyoti Maity <13766712+djmaity@users.noreply.github.com>
2022-02-04 12:54:57 +0530

    Coding for MD DaVis GUI started

--- md_davis/md_davis.py
+++ md_davis/md_davis.py
@@ -1,1 +1,15 @@
+from tkinter import Tk
+from tkinter import ttk
+
+root = Tk()
+root.title('MD DaVis')
+root.iconbitmap('MD_DaVis.ico')
+
+frm = ttk.Frame(root, padding=10)
+frm.grid()
+
+ttk.Label(frm, text="Hello World!").grid(column=0, row=0)
+ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=0)
+
+root.mainloop()
 
