e490a2a489f5e39ab16543fda1c5943c1cad1334
Dibyajyoti Maity <13766712+djmaity@users.noreply.github.com>
2022-02-05 00:53:42 +0530

    Changed GUI from Tkinter to wxPython

--- md_davis/md_davis.py
+++ md_davis/md_davis.py
@@ -1,18 +1,40 @@
-import os
-from tkinter import Tk
-from tkinter import ttk
+import wx
 
-dirname = os.path.dirname(__file__)
 
-root = Tk()
-root.title('MD DaVis')
-root.iconbitmap(os.path.join(dirname, 'MD_DaVis.ico'))
+class MyPanel(wx.Panel):
 
-frm = ttk.Frame(root, padding=10)
-frm.grid()
+    def __init__(self, parent):
+        super().__init__(parent)
+        button = wx.Button(self, label='Get Sequence')
+        button.Bind(wx.EVT_BUTTON, self.on_button_press)
+        main_sizer = wx.BoxSizer(wx.VERTICAL)
+        main_sizer.Add(button, proportion=0, flag=wx.ALL | wx.CENTER, border=5)
+        self.SetSizer(main_sizer)
 
-ttk.Label(frm, text="Hello World!").grid(column=0, row=0)
-ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=0)
+    def on_button_press(self, event):
+        """
+        Browse for a PDB file
+        @param event: The event object
+        """
+        wildcard = "PDB files (*.pdb)|*.pdb"
+        with wx.FileDialog(None, "Choose a file",
+                           wildcard=wildcard,
+                           style=wx.FD_OPEN) as dialog:
+            if dialog.ShowModal() == wx.ID_OK:
+                self.pdb_file.SetValue(dialog.GetPath())
 
-root.mainloop()
 
+class MyFrame(wx.Frame):
+
+    def __init__(self):
+        super().__init__(None, title='MD DaVis')
+        panel = MyPanel(self)
+        self.SetIcon(wx.Icon("MD_DaVis.ico"))
+        self.Center()
+        self.Show()
+
+
+if __name__ == '__main__':
+    app = wx.App()
+    frame = MyFrame()
+    app.MainLoop()
